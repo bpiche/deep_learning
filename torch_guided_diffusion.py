0509c1daf66419fd0e36bfe25d42525de35b3712
bpiche <ben.h.piche@gmail.com>
2022-04-27 23:05:10 -0700

    VQGCAN+CLIP image generation

---
+++ torch_guided_diffusion.py
@@ -0,0 +1,378 @@
+import argparse
+import gc
+import io
+import math
+import sys
+import os
+
+from IPython import display
+import lpips
+from PIL import Image
+import requests
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torchvision import transforms
+from torchvision.transforms import functional as TF
+from tqdm import tqdm
+
+sys.path.append('./CLIP')
+sys.path.append('./guided-diffusion')
+
+import clip
+from guided_diffusion.script_util import create_model_and_diffusion, model_and_diffusion_defaults
+
+# import kornia.augmentation as K
+import matplotlib.pyplot as plt
+import numpy as np
+
+from subprocess import Popen, PIPE, run
+import re
+
+
+vq_parser = argparse.ArgumentParser(description='Image generation using VQGAN+CLIP')
+vq_parser.add_argument("-p",    "--prompts", type=str, help="Text prompts", default="A painting of a nerdy rodent", dest='prompts')
+vq_parser.add_argument("-ip",   "--image_prompts", type=str, help="Image prompts / target image", default=[], dest='image_prompts')
+vq_parser.add_argument("-ii",   "--init_image", type=str, help="Initial image", default=None, dest='init_image')
+vq_parser.add_argument("-st",   "--skip_steps", type=int, help="Skip steps for init image (200-500)", default=0, dest='skip_timesteps') # This needs to be between approx. 200 and 500 when using an init image.
+vq_parser.add_argument("-is",   "--init_scale", type=int, help="Initial image scale (e.g. 1000)", default=0, dest='init_scale') # This enhances the effect of the init image, a good value is 1000.
+vq_parser.add_argument("-m",    "--clip_model", type=str, help="CLIP model (e.g. ViT-B/32, ViT-B/16)", default='ViT-B/16', dest='clip_model')
+vq_parser.add_argument("-t",    "--timesteps", type=str, help="Number of timesteps", default='1000', dest='timesteps') # number(s) (Can be comma separated) or one of ddim25, ddim50, ddim150, ddim250, ddim500, ddim1000 (must be mod0 of diffusion_steps)
+vq_parser.add_argument("-ds",   "--diffusion_steps", type=int, help="Diffusion steps", default=1000, dest='diffusion_steps')
+vq_parser.add_argument("-se",   "--save_every", type=int, help="Image update frequency", default=100, dest='save_every')
+vq_parser.add_argument("-bs",   "--batch_size", type=int, help="Batch size", default=1, dest='batch_size')
+vq_parser.add_argument("-nb",   "--num_batches", type=int, help="Number of batches", default=1, dest='n_batches')
+vq_parser.add_argument("-cuts", "--num_cuts", type=int, help="Number of cuts", default=16, dest='cutn')
+vq_parser.add_argument("-cutb", "--cutn_batches", type=int, help="Number of cut batches", default=2, dest='cutn_batches') # Gradient accumulate every
+vq_parser.add_argument("-cutp", "--cut_power", type=float, help="Cut power", default=1., dest='cut_pow')
+vq_parser.add_argument("-cgs",  "--clip_scale", type=int, help="CLIP guidance scale", default=1000, dest='clip_guidance_scale') # Controls how much the image should look like the prompt.
+vq_parser.add_argument("-tvs",  "--tv_scale", type=float, help="Smoothness scale", default=150, dest='tv_scale') # Controls the smoothness of the final output.
+vq_parser.add_argument("-rgs",  "--range_scale", type=int, help="RGB range scale", default=50, dest='range_scale') # Controls how far out of range RGB values are allowed to be.
+vq_parser.add_argument("-os",   "--output_size", type=int, help="Output image size (256 or 512)", default=256, dest='image_size')
+vq_parser.add_argument("-s",    "--seed", type=int, help="Seed", default=None, dest='seed')
+vq_parser.add_argument("-o",    "--output", type=str, help="Output file", default="output.png", dest='output')
+vq_parser.add_argument("-vid",  "--video", action='store_true', help="Create video frames (steps)?", dest='make_video')
+vq_parser.add_argument("-vup",  "--video_upscale", action='store_true', help="Upscale video? (needs Real-ESRGAN executable)", dest='upscale_video')
+vq_parser.add_argument("-nfp",  "--no_fp16", action='store_false', help="Disable fp16?", dest='use_fp16')
+vq_parser.add_argument("-nbm",  "--no_benchmark", action='store_false', help="Disable CuDNN benchmark?", dest='cudnn_bm')
+vq_parser.add_argument("-pl",   "--plot_loss", action='store_true', help="Plot loss?", dest='graph_loss')
+vq_parser.add_argument("-dev",  "--cuda_device", type=str, help="CUDA Device", default='cuda:0', dest='cuda_device')
+args = vq_parser.parse_args()
+
+if args.image_size != 256 and args.image_size != 512:
+    args.image_size = 256
+
+if args.make_video:
+    if not os.path.exists('steps'):
+        os.mkdir('steps')
+
+if args.cudnn_bm:
+    torch.backends.cudnn.benchmark = True
+
+prompts = [phrase.strip() for phrase in args.prompts.split("|")]
+
+if args.image_prompts:
+    args.image_prompts = args.image_prompts.split("|")
+    args.image_prompts = [image.strip() for image in args.image_prompts]
+
+image_prompts = args.image_prompts
+batch_size = args.batch_size
+clip_guidance_scale = args.clip_guidance_scale
+tv_scale = args.tv_scale
+range_scale = args.range_scale
+cutn = args.cutn
+cutn_batches = args.cutn_batches
+cut_pow = args.cut_pow
+n_batches = args.n_batches
+init_image = args.init_image
+skip_timesteps = args.skip_timesteps
+init_scale = args.init_scale
+seed = args.seed
+
+model_config = model_and_diffusion_defaults()
+model_config.update({
+    'attention_resolutions': '32, 16, 8',
+    'class_cond': False,
+    'diffusion_steps': args.diffusion_steps,
+    'rescale_timesteps': True,
+    'timestep_respacing': args.timesteps,
+    'image_size': args.image_size,
+    'learn_sigma': True,
+    'noise_schedule': 'linear',
+    'num_channels': 256,
+    'num_head_channels': 64,
+    'num_res_blocks': 2,
+    'resblock_updown': True,
+    'use_fp16': args.use_fp16,
+    'use_scale_shift_norm': True,
+})
+
+device = torch.device(args.cuda_device if torch.cuda.is_available() else 'cpu')
+print('Device:', device)
+print('Size: ', args.image_size)
+
+model, diffusion = create_model_and_diffusion(**model_config)
+if args.image_size == 256:
+    model.load_state_dict(torch.load('256x256_diffusion_uncond.pt', map_location=device))
+else:
+    model.load_state_dict(torch.load('512x512_diffusion_uncond_finetune_008100.pt', map_location=device))
+
+model.requires_grad_(False).eval().to(device)
+for name, param in model.named_parameters():
+    if 'qkv' in name or 'norm' in name or 'proj' in name:
+        param.requires_grad_()
+if model_config['use_fp16']:
+    model.convert_to_fp16()
+
+clip_model = clip.load(args.clip_model, jit=False)[0].eval().requires_grad_(False).to(device)
+clip_size = clip_model.visual.input_resolution
+normalize = transforms.Normalize(mean=[0.48145466, 0.4578275, 0.40821073], 
+                                std=[0.26862954, 0.26130258, 0.27577711])
+lpips_model = lpips.LPIPS(net='vgg').to(device)
+
+
+def fetch(url_or_path):
+    """
+    """
+    if str(url_or_path).startswith('http://') or str(url_or_path).startswith('https://'):
+        r = requests.get(url_or_path)
+        r.raise_for_status()
+        fd = io.BytesIO()
+        fd.write(r.content)
+        fd.seek(0)
+        return fd
+    return open(url_or_path, 'rb')
+
+
+def parse_prompt(prompt):
+    """
+    """
+    if prompt.startswith('http://') or prompt.startswith('https://'):
+        vals = prompt.rsplit(':', 2)
+        vals = [vals[0] + ':' + vals[1], *vals[2:]]
+    else:
+        vals = prompt.rsplit(':', 1)
+    vals = vals + ['', '1'][len(vals):]
+    return vals[0], float(vals[1])
+
+
+class MakeCutouts(nn.Module):
+    """
+    """
+    def __init__(self, cut_size, cutn, cut_pow=1.):
+        super().__init__()
+        self.cut_size = cut_size
+        self.cutn = cutn
+        self.cut_pow = cut_pow
+
+    def forward(self, input):
+        sideY, sideX = input.shape[2:4]
+        max_size = min(sideX, sideY)
+        min_size = min(sideX, sideY, self.cut_size)
+        cutouts = []
+        for _ in range(self.cutn):
+            size = int(torch.rand([])**self.cut_pow * (max_size - min_size) + min_size)
+            offsetx = torch.randint(0, sideX - size + 1, ())
+            offsety = torch.randint(0, sideY - size + 1, ())
+            cutout = input[:, :, offsety:offsety + size, offsetx:offsetx + size]
+            cutouts.append(F.adaptive_avg_pool2d(cutout, self.cut_size))
+        return torch.cat(cutouts)
+
+
+def spherical_dist_loss(x, y):
+    """
+    """
+    x = F.normalize(x, dim=-1)
+    y = F.normalize(y, dim=-1)
+    return (x - y).norm(dim=-1).div(2).arcsin().pow(2).mul(2)
+
+
+def tv_loss(input):
+    """
+    L2 total variation loss, as in Mahendran et al.
+    """
+    input = F.pad(input, (0, 1, 0, 1), 'replicate')
+    x_diff = input[..., :-1, 1:] - input[..., :-1, :-1]
+    y_diff = input[..., 1:, :-1] - input[..., :-1, :-1]
+    return (x_diff**2 + y_diff**2).mean([1, 2, 3])
+
+
+def range_loss(input):
+    """
+    """
+    return (input - input.clamp(-1, 1)).pow(2).mean([1, 2, 3])
+
+
+def do_run():
+    if args.seed is None:
+        seed = torch.seed()
+    else:
+        seed = args.seed
+    torch.manual_seed(seed)
+    print("Seed:", seed)
+    loss_test = []
+    make_cutouts = MakeCutouts(clip_size, cutn, cut_pow)
+    side_x = side_y = model_config['image_size']
+    target_embeds, weights = [], []
+    for prompt in prompts:
+        print('Text prompt:', prompt)
+        txt, weight = parse_prompt(prompt)
+        target_embeds.append(clip_model.encode_text(clip.tokenize(txt).to(device)).float())
+        weights.append(weight)
+    for prompt in image_prompts:
+        print('Image prompt:', prompt)
+        path, weight = parse_prompt(prompt)
+        img = Image.open(fetch(path)).convert('RGB')
+        img = TF.resize(img, min(side_x, side_y, *img.size), transforms.InterpolationMode.LANCZOS)
+        batch = make_cutouts(TF.to_tensor(img).unsqueeze(0).to(device))
+        embed = clip_model.encode_image(normalize(batch)).float()
+        target_embeds.append(embed)
+        weights.extend([weight / cutn] * cutn)
+    target_embeds = torch.cat(target_embeds)
+    weights = torch.tensor(weights, device=device)
+    if weights.sum().abs() < 1e-3:
+        raise RuntimeError('The weights must not sum to 0.')
+    weights /= weights.sum().abs()
+    init = None
+    if init_image is not None:
+        print('Initial image:', init_image)
+        init = Image.open(fetch(init_image)).convert('RGB')
+        init = init.resize((side_x, side_y), Image.LANCZOS)
+        init = TF.to_tensor(init).to(device).unsqueeze(0).mul(2).sub(1)
+    cur_t = None
+
+    def cond_fn(x, t, y=None):
+        with torch.enable_grad():
+            x = x.detach().requires_grad_()
+            n = x.shape[0]
+            my_t = torch.ones([n], device=device, dtype=torch.long) * cur_t
+            out = diffusion.p_mean_variance(model, x, my_t, clip_denoised=False, model_kwargs={'y': y})
+            fac = diffusion.sqrt_one_minus_alphas_cumprod[cur_t]
+            x_in = out['pred_xstart'] * fac + x * (1 - fac)
+            x_in_grad = torch.zeros_like(x_in)
+            # Encode image and calculate spherical distance loss
+            for i in range(cutn_batches):
+                clip_in = normalize(make_cutouts(x_in.add(1).div(2)))
+                image_embeds = clip_model.encode_image(clip_in).float()
+                dists = spherical_dist_loss(image_embeds.unsqueeze(1), target_embeds.unsqueeze(0))
+                dists = dists.view([cutn, n, -1])
+                losses = dists.mul(weights).sum(2).mean(0)
+                # Saving loss for plot
+                if args.graph_loss:
+                    loss_test.append(losses.sum().item())
+                x_in_grad += torch.autograd.grad(losses.sum() * clip_guidance_scale, x_in)[0] / cutn_batches
+            tv_losses = tv_loss(x_in)
+            range_losses = range_loss(out['pred_xstart'])
+            loss = (tv_losses.sum() * tv_scale) + (range_losses.sum() * range_scale)
+            if init is not None and init_scale:
+                init_losses = lpips_model(x_in, init)
+                loss = loss + init_losses.sum() * init_scale
+            x_in_grad += torch.autograd.grad(loss, x_in)[0]
+            grad = -torch.autograd.grad(x_in, x, x_in_grad)[0]
+            return grad
+    if model_config['timestep_respacing'].startswith('ddim'):
+        sample_fn = diffusion.ddim_sample_loop_progressive
+    else:
+        sample_fn = diffusion.p_sample_loop_progressive
+    for i in range(n_batches):
+        cur_t = diffusion.num_timesteps - skip_timesteps - 1
+        samples = sample_fn(
+            model,
+            (batch_size, 3, side_y, side_x),
+            clip_denoised=False,
+            model_kwargs={},
+            cond_fn=cond_fn,
+            progress=True,
+            skip_timesteps=skip_timesteps,
+            init_image=init,
+            randomize_class=True,
+        )
+        for j, sample in enumerate(samples):
+            if j % args.save_every == 0 or cur_t == 0:
+                print()
+                for k, image in enumerate(sample['pred_xstart']):
+                    # filename = f'progress_{i * batch_size + k:05}.png'
+                    # TF.to_pil_image(image.add(1).div(2).clamp(0, 1)).save(filename)
+                    if i > 0:
+                        b_filename = (os.path.basename(args.output).split('.')[0])
+                        b_filename = b_filename + '-' + str(i) + '.png'
+                        b_filename = os.path.join(os.path.dirname(args.output), b_filename)
+                    else:
+                        b_filename = args.output
+                    TF.to_pil_image(image.add(1).div(2).clamp(0, 1)).save(b_filename)
+                    tqdm.write(f'Batch {i}, step {j}, output {k}:')
+                    # display.display(display.Image(filename))
+                if args.graph_loss:
+                    plt.plot(np.array(loss_test), 'r')
+                    plot_filename = (os.path.basename(args.output).split('.')[0])
+                    plot_filename = plot_filename + '-' + str(i) + '-loss.jpg'
+                    plot_filename = os.path.join(os.path.dirname(args.output), plot_filename)
+                    plt.savefig(plot_filename)
+            if args.make_video:
+                for k, image in enumerate(sample['pred_xstart']):
+                    step_filename = str(j) + '.png'
+                    frame_filename = os.path.join('steps', step_filename)
+                    TF.to_pil_image(image.add(1).div(2).clamp(0, 1)).save(frame_filename)
+            cur_t -= 1
+        # Video generation
+        if args.make_video:
+            init_frame = 1      # Initial video frame
+            last_frame = j      # This will raise an error if that number of frames does not exist.
+            length = 5          # Desired time of the video in seconds
+            min_fps = 10
+            max_fps = 60
+            total_frames = last_frame-init_frame
+            if args.upscale_video:
+                if not os.path.exists('upscaled_steps'):
+                    os.mkdir('upscaled_steps')
+                try:
+                    run("./realesrgan-ncnn-vulkan -i steps -o upscaled_steps", check=True, shell=True)
+                except FileNotFoundError:
+                    print("realesrgan-ncnn-vulkan not found")
+                    args.upscale_video = False
+            frames = []
+            tqdm.write('Generating video...')
+            for k in range(init_frame,last_frame):
+                if args.upscale_video:
+                    temp = Image.open("./upscaled_steps/"+ str(k) +'.png')
+                else:
+                    temp = Image.open("./steps/"+ str(k) +'.png')
+                keep = temp.copy()
+                frames.append(keep)
+                temp.close()
+            fps = np.clip(total_frames/length,min_fps,max_fps)
+            # Batches
+            if i > 0:
+                m_filename = (os.path.basename(args.output).split('.')[0])
+                m_filename = m_filename + '-' + str(i) + '.png'
+                m_filename = os.path.join(os.path.dirname(args.output), m_filename)
+            else:
+                m_filename = args.output
+            output_file = re.compile('\.png$').sub('.mp4', m_filename)
+            try:
+                p = Popen(['ffmpeg',
+                           '-y',
+                           '-f', 'image2pipe',
+                           '-vcodec', 'png',
+                           '-r', str(fps),
+                           '-i',
+                           '-',
+                           '-vcodec', 'libx264',
+                           '-r', str(fps),
+                           '-pix_fmt', 'yuv420p',
+                           '-crf', '17',
+                           '-preset', 'veryslow',
+                           output_file], stdin=PIPE)
+            except FileNotFoundError:
+                print("ffmpeg command failed - check your installation")
+            for im in tqdm(frames):
+                im.save(p.stdin, 'PNG')
+            p.stdin.close()
+            p.wait()
+
+
+if __name__ == "__main__":
+    """
+    """
+    gc.collect()
+    do_run()
--- torch_image_classification.py
+++ torch_image_classification.py
@@ -1,3 +1,4 @@
+import warnings
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -10,50 +11,27 @@ import torch.nn.functional as F
 
 import torch.optim as optim
 
-
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-batch_size = 4
-
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                          shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                         shuffle=False, num_workers=2)
-
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+warnings.filterwarnings("ignore")
 
 
 def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
+    """
+    """
+    img = img / 2 + 0.5
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
 
-# get some random training images
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
-
-# show images
-imshow(torchvision.utils.make_grid(images))
-# print labels
-print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
-
-
 class Net(nn.Module):
+    """
+    """
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        depth = 60
+        self.conv1 = nn.Conv2d(3, depth, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv2 = nn.Conv2d(depth, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
@@ -68,94 +46,110 @@ class Net(nn.Module):
         return x
 
 
-net = Net()
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-net.to(device)
-
-
-
-for epoch in range(2):  # loop over the dataset multiple times
-
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        # inputs, labels = data
-        inputs, labels = data[0].to(device), data[1].to(device)
-
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            running_loss = 0.0
-
-print('Finished Training')
-
-PATH = './cifar_net.pth'
-torch.save(net.state_dict(), PATH)
-
-dataiter = iter(testloader)
-images, labels = dataiter.next()
-
-# print images
-imshow(torchvision.utils.make_grid(images))
-print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
-
-net = Net()
-net.load_state_dict(torch.load(PATH))
-outputs = net(images)
-
-_, predicted = torch.max(outputs, 1)
-
-print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}'
-                              for j in range(4)))
-
-correct = 0
-total = 0
-# since we're not training, we don't need to calculate the gradients for our outputs
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        # calculate outputs by running images through the network
-        outputs = net(images)
-        # the class with the highest energy is what we choose as prediction
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
-
-# prepare to count predictions for each class
-correct_pred = {classname: 0 for classname in classes}
-total_pred = {classname: 0 for classname in classes}
-
-# again no gradients needed
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = net(images)
-        _, predictions = torch.max(outputs, 1)
-        # collect the correct predictions for each class
-        for label, prediction in zip(labels, predictions):
-            if label == prediction:
-                correct_pred[classes[label]] += 1
-            total_pred[classes[label]] += 1
+def train_model(trainloader, epochs=10):
+    """
+    """
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data[0].to(device), data[1].to(device)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+    print('Finished Training')
+    torch.save(net.state_dict(), PATH)
 
 
-# print accuracy for each class
-for classname, correct_count in correct_pred.items():
-    accuracy = 100 * float(correct_count) / total_pred[classname]
-    print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
+if __name__ == "__main__":
+    """
+    """
+    # TODO: turn the test script into a function
+    # TODO: use other datasets and test cases
+    net = Net()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    net.to(device)
+
+    transform = transforms.Compose(
+                [transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    epochs = 10
+    batch_size = 4
+    PATH = './cifar_net.pth'
+
+    # prepare training set
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=2)
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    # train classifier
+    train_model(trainloader, epochs)
+
+    # prepare test data set
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                        download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=2)
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck')
+
+    # load trained model and predict a sample
+    net = Net()
+    net.load_state_dict(torch.load(PATH))
+    outputs = net(images)
+    _, predicted = torch.max(outputs, 1)
+    print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}'
+                                for j in range(4)))
+
+    # calculate accuracy against a test set
+    correct = 0
+    total = 0
+    # since we're not training, we don't need to calculate the gradients for our outputs
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            # calculate outputs by running images through the network
+            outputs = net(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+    # prepare to count predictions for each class
+    correct_pred = {classname: 0 for classname in classes}
+    total_pred = {classname: 0 for classname in classes}
+    # again no gradients needed
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predictions = torch.max(outputs, 1)
+            # collect the correct predictions for each class
+            for label, prediction in zip(labels, predictions):
+                if label == prediction:
+                    correct_pred[classes[label]] += 1
+                total_pred[classes[label]] += 1
+    # print accuracy for each class
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 * float(correct_count) / total_pred[classname]
+        print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
